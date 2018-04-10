f10128f5608b6b507479a152877d82a0cc87971e
nataly <angynk17@gmail.com>
2018-04-09 21:14:10 -0500

    Ajustes Cloud

--- src/main/java/com/tmModulos/controlador/utils/PathFiles.java
+++ src/main/java/com/tmModulos/controlador/utils/PathFiles.java
@@ -2,9 +2,9 @@ package com.tmModulos.controlador.utils;
 
 public class PathFiles {
 
- public static final String PATH_FOR_FILES = "C:\\temp-programacion";
+// public static final String PATH_FOR_FILES = "C:\\temp-programacion";
 
-    //public static final String PATH_FOR_FILES = "\\home\\angynk7\\temp";
+    public static final String PATH_FOR_FILES = "/usr/local/temp";
 // public static final String PATH_FOR_FILES = "M:\\temp";
 
    public static final String BUSES_HORA_FILE = "busesHora.xls";
